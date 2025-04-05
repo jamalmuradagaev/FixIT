489e8cf90ec3962f7b418c910dda1aab9ed5e1af
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2025-04-05 22:41:48 +0300

    Страница регистрации

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,9 +1,35 @@
-import s from '../styles/pages/_Registration.module.scss'
+import s from '../styles/pages/_Authorization.module.scss'
+import { useNavigate } from 'react-router'
+import { useState } from 'react'
+import Input from '../components/Input'
+import logo from '../../public/logo.png'
 
 const RegistrationPage = () => {
+    const navigate = useNavigate()
+    const [name, setName] = useState<string>()
+    const [login, setLogin] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+
+    console.log({ name, login, password })
+
     return (
         <div>
-            
+            <img src={logo} alt="" />
+
+            <h2>Создайте свою <br /> учетную запись</h2>
+
+            <form action="">
+                <p>Имя</p>
+                <Input placeholder='Армаслисурхан' value={name} onChange={(e: any) => setName(e.target.value)} name='name'></Input>
+                <p>Логин</p>
+                <Input placeholder='Номер телефона' value={login} onChange={(e: any) => setLogin(e.target.value)} name='login'></Input>
+                <p>Пароль</p>
+                <Input placeholder='*******' value={password} onChange={(e: any) => setPassword(e.target.value)} name='password'></Input>
+                <button>Создать</button>
+            </form>
+
+
+            <p>Уже есть аккаунт? <a onClick={() => navigate('/authorization')}>Войти</a></p>
         </div>
     )
 }
