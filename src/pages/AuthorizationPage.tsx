e9334e072f3ddf493882c0cc002223055ecde918
Sabina Gasanova <sabina21gasanova@yandex.ru>
2025-08-10 12:43:00 +0300

    add form validation

--- src/pages/AuthorizationPage.tsx
+++ src/pages/AuthorizationPage.tsx
@@ -1,23 +1,36 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { useDispatch } from 'react-redux'
+import { useForm, Controller } from 'react-hook-form'
+
 import s from '../styles/pages/_Authorization.module.scss'
 import logo from '../../public/logo.png'
+
 import Input from '../components/Input'
-import { useNavigate } from 'react-router'
-import { useState } from 'react'
-import { TokenUtils } from '../utils/TokenUtils'
-import { useDispatch } from 'react-redux'
-import { setAccessToken, setIsAuthorized, setRefreshToken } from '../store/userSlice'
 import Button from '../components/Button'
 
+
+import { TokenUtils } from '../utils/TokenUtils'
+import { setAccessToken, setIsAuthorized, setRefreshToken } from '../store/userSlice'
+
+
+type FormData = {
+    email: string;
+    password: string;
+}
+
 const AuthorizationPage = () => {
+
+    const {handleSubmit, control, formState: {errors}} = useForm<FormData>({defaultValues: { email: '', password: ''}})
+
+    const [failed, setFailed] = useState('');
+    
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const [login, setLogin] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
 
-    console.log({login, password})
 
-    const handleLogin = async(e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
+    const handleLogin = async(formData: FormData) => {
+
         try {
             const response = await fetch('http://188.120.240.237:8000/api/token/', {
                 method: 'POST',
@@ -26,13 +39,15 @@ const AuthorizationPage = () => {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    email: login,
-                    password: password
+                    email: formData.email,
+                    password: formData.password
                 })
             })
     
             if (!response.ok) {
-                throw new Error('Не удалось войти в систему')
+                setFailed('Неверно введен логин или пароль')
+                throw new Error('Не удалось войти в систему');
+                
             }
     
             const token = await response.json();
@@ -46,6 +61,7 @@ const AuthorizationPage = () => {
             navigate('/');
         }
         catch (error) {
+            setFailed('Неверно введен логин или пароль');
             console.log(error)
         }
     }
@@ -56,12 +72,50 @@ const AuthorizationPage = () => {
 
             <h2 className={s.descriptionForm}>Войдите в свою <br /> учетную запись</h2>
 
-            <form action="" className={s.startForm} onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleLogin(e)}>
+            <form className={s.startForm} onSubmit={handleSubmit(handleLogin)}>
                 <p>Логин</p>
-                <Input placeholder='e-mail' type={'email'} value={login} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLogin(e.target.value)} name='login'></Input>
+                <Controller 
+                    name='email'
+                    rules={
+                        {required: 'Заполните поле',
+                         pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: 'Введите почту правильно, пример - example@mail.ru'
+                         }
+                        }
+                    }
+                    control={control}
+                    render={({field: {onChange, value}}) =>
+                     <Input placeholder='e-mail' type={'email'} value={value} onChange={(e) => {onChange(e); setFailed('')}} name='login'></Input> }
+                />
+                  {errors.email && (
+                    <span className={s.error}>{errors.email.message?.toString()}</span>
+                )}
                 <p>Пароль</p>
-                <Input placeholder='*******' type={'password'} value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} name='password' isPassword={true}></Input>
+                <Controller 
+                    name='password'
+                    rules={
+                        {required: 'Заполните поле',
+                         minLength: {
+                            value: 6,
+                            message: 'Пароль должен состоять из 6 или более символов'
+                         }
+                        }
+                    }
+                    control={control}
+                    render={({field: {onChange, value}}) =>
+                     <Input placeholder='*******' type={'password'} value={value} onChange={(e) => {onChange(e); setFailed('')}} name='password' isPassword={true}></Input>
+                    }
+                 />
+                 {errors.password && (
+                    <span className={s.error}>{errors.password.message?.toString()}</span>
+                    )}
+               
                 <p className={s.lostPassword} onClick={() => navigate('/recovery1')}>Забыли пароль?</p>
+
+                 {!!failed && (
+                    <span className={s.failed}>{failed}</span>
+                )}
                 <Button type='submit'>Войти</Button>
             </form>
 
